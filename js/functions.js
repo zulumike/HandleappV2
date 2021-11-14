d8d592ef4a8e76a1948e19d5f480601fca596396
Ole Kristian Settem <ole@zmmaskin.no>
2021-11-14 23:29:13 +0100

    Div retting av plasseringer. scrolltotop function vha js

---
+++ js/functions.js
@@ -0,0 +1,24 @@
+//Get the button
+let toTopButton = document.getElementById("toTopButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    toTopButton.style.display = "block";
+  } else {
+    toTopButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+  window.scroll({
+    top: 0, 
+    left: 0, 
+    behavior: 'smooth'
+  });
+}
