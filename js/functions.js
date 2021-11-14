a46400cda2ce3e065e58c041478078674d7b5aca
Ole Kristian Settem <ole@zmmaskin.no>
2021-11-14 23:37:39 +0100

    Laget global variabel for max-width og minsket maks bredden

--- js/functions.js
+++ js/functions.js
@@ -14,8 +14,6 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-//   document.body.scrollTop = 0;
-//   document.documentElement.scrollTop = 0;
   window.scroll({
     top: 0, 
     left: 0, 
