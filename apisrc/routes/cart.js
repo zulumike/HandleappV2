e66ce7a68f841b0a60c42f4721e6d5bb6f337aab
Ole Kristian Settem <ole@zmmaskin.no>
2022-09-17 13:09:27 +0200

    Deleted apisrc

--- apisrc/routes/cart.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const cartController = require('../controllers/cart');
-
-const router = express.Router();
-
-router.post('/post', cartController.dbWrite2);
-
-module.exports = router;
